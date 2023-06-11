f25cb03f7e1d1c31adcb4fe20edd287a4f24fe21
b4iterdev <thaimeo200389@gmail.com>
2023-06-11 12:09:58 +0700

    remove Web Panel to switch to dev branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,6 @@
 #include <Arduino.h>
 #include "motors.h"
 #include "PS2_controller.h"
-#include <DNSServer.h>
-#include <ESPUI.h>
-#include <Wire.h>
-
-const byte DNS_PORT = 53;
-IPAddress apIP(192, 168, 4, 1);
-DNSServer dnsServer;
-#include <WiFi.h>
-
-const char *ssid = "STEMIST MSE";
-const char *password = "stemistclub";
-
-const char *hostname = "stemist.mse";
 
 /*
  * A better version of MakerBotwPS2 example used with makerbot BANHMI hardware platform
@@ -30,10 +17,6 @@ const char *hostname = "stemist.mse";
     - Add additional motor control and servo control.
   Written by B4iter - STEMIST MSE Gen 5
 */
-
-void generalCallback(Control *sender, int type) {
-
-}
 
 // define motor 3 and 4.
 #define PWM_CHANNEL5 12
@@ -62,54 +45,6 @@ void setup()
   Serial.begin(115200);
   initMotors();
   setupPS2controller();
-  ESPUI.setVerbosity(Verbosity::VerboseJSON);
-  WiFi.setHostname(hostname);
-  WiFi.begin(ssid, password);
-  Serial.print("\n\nTry to connect to existing network");
-  {
-    uint8_t timeout = 10;
-    // Wait for connection, 5s timeout
-    do
-    {
-      delay(500);
-      Serial.print(".");
-      timeout--;
-    } while (timeout && WiFi.status() != WL_CONNECTED);
-
-    // not connected -> create hotspot
-    if (WiFi.status() != WL_CONNECTED)
-    {
-      Serial.print("\n\nCreating hotspot");
-
-      WiFi.mode(WIFI_AP);
-      WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-      WiFi.softAP(ssid);
-
-      timeout = 5;
-
-      do
-      {
-        delay(500);
-        Serial.print(".");
-        timeout--;
-      } while (timeout);
-    }
-  }
-
-  dnsServer.start(DNS_PORT, "*", apIP);
-
-  Serial.println("\n\nWiFi parameters:");
-  Serial.print("Mode: ");
-  Serial.println(WiFi.getMode() == WIFI_AP ? "Station" : "Client");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.getMode() == WIFI_AP ? WiFi.softAPIP() : WiFi.localIP());
-  // Debug Panel Begin here. 
-  auto maintab = ESPUI.addControl(Tab, "Debug Info", "Debug Info");
-  ESPUI.addControl(Label, "Status", "System status: OK", Alizarin, maintab, generalCallback);
-  ESPUI.addControl(Label, "Control PWM Status:", "PWM1: " + pwm.getPWM(8), Alizarin, maintab, generalCallback);
-	//Make sliders continually report their position as they are being dragged.
-	ESPUI.sliderContinuous = true;
-  ESPUI.begin("STEMIST MSE - VRC 2023 Debug Panel");
   Serial.println("Done setup!");
 }
 
@@ -132,7 +67,6 @@ void delayedStart(unsigned int PWM_CHANNEL) {
 
 void loop()
 {
-  dnsServer.processNextRequest();
   ps2x.read_gamepad(0, 0);
   PS2control();
   if (ps2x.ButtonReleased(PSB_CROSS) || ps2x.ButtonReleased(PSB_CIRCLE)) {
