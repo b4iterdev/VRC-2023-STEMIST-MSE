cd3f3960e9d763203bc36a27aad30d7609045eeb
b4iterdev <thaimeo200389@gmail.com>
2023-06-11 11:56:21 +0700

    PS2 fixup and Web Panel WIP

--- include/PS2_controller.h
+++ include/PS2_controller.h
@@ -2,6 +2,9 @@
 
 PS2X ps2x; // create PS2 Controller Class object
 
+// calibration for different kinds of PS2 controller, this value only suitable for the PS2 controller comes with VRC2023 K12 Maker kit 
+#define X_JOY_CALIB 127
+#define Y_JOY_CALIB 128
 #define PS2_DAT 12 // MISO  19
 #define PS2_CMD 13 // MOSI  23
 #define PS2_SEL 15 // SS     5
@@ -20,10 +23,18 @@ void setupPS2controller()
 bool PS2control()
 {
 
-  int nJoyX = 128 - ps2x.Analog(PSS_RX); // read x-joystick
-  int nJoyY = 128 - ps2x.Analog(PSS_LY); // read y-joystick
+  // int nJoyX = 128 - ps2x.Analog(PSS_RX); // read x-joystick
+  // int nJoyY = 128 - ps2x.Analog(PSS_LY); // read y-joystick
+  int nJoyX = X_JOY_CALIB - ps2x.Analog(PSS_RX); // read x-joystick
+  int nJoyY = Y_JOY_CALIB - ps2x.Analog(PSS_LY); // read y-joystick
   int nMotMixL;                          // Motor (left) mixed output
   int nMotMixR;                          // Motor (right) mixed output
+
+  if(nJoyX == -1 && nJoyY == 0) // in case of lost connection with the wireless controller, only used in VRC2023 PS2 wireless controller 
+  {
+    setPWMMotors(0, 0, 0, 0);
+    return 0;
+  }
 
   bool temp = (nJoyY * nJoyX > 0);
   if (nJoyX) // Turning
@@ -41,7 +52,7 @@ bool PS2control()
   // Serial.print(nMotMixL);
   // Serial.print("\t");
   // Serial.println(nMotMixR);
-  int c1 = 0, c2 = 0, c3 = 0, c4 = 0;
+   int c1 = 0, c2 = 0, c3 = 0, c4 = 0;
 
   if (nMotMixR > 0)
   {
@@ -62,7 +73,7 @@ bool PS2control()
   }
   else if (nMotMixL < 0)
   {
-    c2 = abs(nMotMixL)+1;
+    c2 = abs(nMotMixL);
     c2 = map(c2, 0, 128, 0, SPEED);
   }
   setPWMMotors(c1, c2, c3, c4);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <Arduino.h>
 #include "motors.h"
 #include "PS2_controller.h"
+#include <DNSServer.h>
+#include <ESPUI.h>
+#include <Wire.h>
+
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 4, 1);
+DNSServer dnsServer;
+#include <WiFi.h>
+
+const char *ssid = "STEMIST MSE";
+const char *password = "stemistclub";
+
+const char *hostname = "stemist.mse";
 
 /*
  * A better version of MakerBotwPS2 example used with makerbot BANHMI hardware platform
@@ -17,6 +30,10 @@
     - Add additional motor control and servo control.
   Written by B4iter - STEMIST MSE Gen 5
 */
+
+void generalCallback(Control *sender, int type) {
+
+}
 
 // define motor 3 and 4.
 #define PWM_CHANNEL5 12
@@ -45,13 +62,61 @@ void setup()
   Serial.begin(115200);
   initMotors();
   setupPS2controller();
+  ESPUI.setVerbosity(Verbosity::VerboseJSON);
+  WiFi.setHostname(hostname);
+  WiFi.begin(ssid, password);
+  Serial.print("\n\nTry to connect to existing network");
+  {
+    uint8_t timeout = 10;
+    // Wait for connection, 5s timeout
+    do
+    {
+      delay(500);
+      Serial.print(".");
+      timeout--;
+    } while (timeout && WiFi.status() != WL_CONNECTED);
+
+    // not connected -> create hotspot
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.print("\n\nCreating hotspot");
+
+      WiFi.mode(WIFI_AP);
+      WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+      WiFi.softAP(ssid);
+
+      timeout = 5;
+
+      do
+      {
+        delay(500);
+        Serial.print(".");
+        timeout--;
+      } while (timeout);
+    }
+  }
+
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  Serial.println("\n\nWiFi parameters:");
+  Serial.print("Mode: ");
+  Serial.println(WiFi.getMode() == WIFI_AP ? "Station" : "Client");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.getMode() == WIFI_AP ? WiFi.softAPIP() : WiFi.localIP());
+  // Debug Panel Begin here. 
+  auto maintab = ESPUI.addControl(Tab, "Debug Info", "Debug Info");
+  ESPUI.addControl(Label, "Status", "System status: OK", Alizarin, maintab, generalCallback);
+  ESPUI.addControl(Label, "Control PWM Status:", "PWM1: " + pwm.getPWM(8), Alizarin, maintab, generalCallback);
+	//Make sliders continually report their position as they are being dragged.
+	ESPUI.sliderContinuous = true;
+  ESPUI.begin("STEMIST MSE - VRC 2023 Debug Panel");
   Serial.println("Done setup!");
 }
 
 /*
   This funtion will make selected PWM Channel to start from 1024 on first stage and full speed when reached 1 second of runtime.
 */
-int delayedStart(unsigned int PWM_CHANNEL) {
+void delayedStart(unsigned int PWM_CHANNEL) {
   long long time = millis();
   if (millis() - time <= 200) {
     pwm.setPWM(PWM_CHANNEL,0,1024);
@@ -61,12 +126,13 @@ int delayedStart(unsigned int PWM_CHANNEL) {
     pwm.setPWM(PWM_CHANNEL,0,3072);
   } else if (millis() - time >= 1000) {
     pwm.setPWM(PWM_CHANNEL,0,4095);
-    return 0;
+    return;
   } 
 }
 
 void loop()
 {
+  dnsServer.processNextRequest();
   ps2x.read_gamepad(0, 0);
   PS2control();
   if (ps2x.ButtonReleased(PSB_CROSS) || ps2x.ButtonReleased(PSB_CIRCLE)) {
