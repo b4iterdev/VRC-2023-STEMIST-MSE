2c0fb43591fa0190f5c465664c16598c36fbadff
b4iterdev <thaimeo200389@gmail.com>
2023-07-11 18:51:01 +0700

    add control for seperate motor

--- src/main.cpp
+++ src/main.cpp
@@ -42,11 +42,12 @@
 #define PWM_CHANNEL7 14
 #define PWM_CHANNEL8 15
 
-uint16_t PWMMotor1s1,PWMMotor1s2,PWMMotor2s1,PWMMotor2s2,servo1pos,PWMMotor3s1,PWMMotor3s2,PWMMotor4s1,PWMMotor4s2,sliMaxMotor,addMotorControl,configWarning,disabledWarning,authorsection,creditsection,teamsection1,teamsection2,specialthanks,boardAction,ultraMainSwitch,ultraDistance,maxThresholdControl1,minThresholdControl1,maxThresholdVerbose1,minThresholdVerbose1,desiredDistance1,laserStatus;
+uint16_t PWMMotor1s1,PWMMotor1s2,PWMMotor2s1,PWMMotor2s2,servo1pos,PWMMotor3s1,PWMMotor3s2,PWMMotor4s1,PWMMotor4s2,sliMaxMotor,addMotorControl3,addMotorControl4,configWarning,disabledWarning,authorsection,creditsection,teamsection1,teamsection2,specialthanks,boardAction,ultraMainSwitch,ultraDistance,maxThresholdControl1,minThresholdControl1,maxThresholdVerbose1,minThresholdVerbose1,desiredDistance1,laserStatus;
 
 bool pressed1 = 0,pressed2 = 0;
 // define adjustable additional Motor Speed.
-unsigned int addMotorSpeed = 4095;
+unsigned int addMotorSpeed3 = 3109;
+unsigned int addMotorSpeed4 = 4095;
 // define desired servo position, it must be configured manually and applied before duty cycle implementation.
 int servo1_pos,servo2_pos,servo3_pos,servo4_pos,servo5_pos,servo6_pos;
 // duty cycle implementation.
@@ -68,22 +69,31 @@ const char *hostname = "stemist.mse";
 //This funtion will change addMotorSpeed varible as user change its value on slider.
 void addMotorControlCallback(Control *sender, int type) {
   Serial.print("Slider: ID: ");
+  int id = sender->id;
   Serial.print(sender->id);
   Serial.print(", Value: ");
   int switchval = sender->value.toInt();
+  switch(id) {
+    case 31:
+      addMotorSpeed3 = switchval;
+    break;
+    case 34:
+      addMotorSpeed4 = switchval;
+    break;
+  }  
   Serial.print(switchval);
-  addMotorSpeed = switchval;
 }
+
 //This funtion will react to ESP Action Button.
 void espActionCallback(Control *sender, int type) {
   Serial.print("Button: ID: ");
   Serial.print(sender->id);
   switch (sender->id) {
-    case 33:
+    case 37:
     Serial.print("ESP is now cleanly restarting...");
     ESP.restart();
     break;
-    case 34:
+    case 38:
     Serial.print("PCA9685 is now resetting...");
     pwm.reset();
     break;
@@ -109,11 +119,11 @@ void thresholdCallback(Control *sender, int type) {
   float switchval = sender->value.toFloat();
   Serial.print(switchval);
   switch (sender->id) {
-    case 43:
+    case 46:
       minThreshold1 = switchval;
       ESPUI.updateLabel(minThresholdVerbose1,String(minThreshold1));
     break;
-    case 44:
+    case 49:
       maxThreshold1 = switchval;
       ESPUI.updateLabel(maxThresholdVerbose1,String(maxThreshold1));
     break;
@@ -168,10 +178,14 @@ void initPanel() {
   disabledWarning = ESPUI.addControl(Label,"Warning","This configuration tab is disabled from controller <br> Please reboot the machine to unlock it",ControlColor::Alizarin,configtab);
   configWarning = ESPUI.addControl(Label,"Warning","Only use this if you know what you're doing",ControlColor::Alizarin,configtab);
   ESPUI.setPanelWide(configWarning, true);
-  addMotorControl = ESPUI.addControl(ControlType::Slider, "Additional Motor Speed", String(addMotorSpeed), Peterriver, configtab, addMotorControlCallback);
-  ESPUI.addControl(Min, "", "0", None, addMotorControl);
-  ESPUI.addControl(Max, "", "4095", None, addMotorControl);
-  ESPUI.setPanelWide(addMotorControl, true);
+  addMotorControl3 = ESPUI.addControl(ControlType::Slider, "Additional Motor 3 Speed", String(addMotorSpeed3), Peterriver, configtab, addMotorControlCallback);
+  ESPUI.addControl(Min, "", "0", None, addMotorControl3);
+  ESPUI.addControl(Max, "", "4095", None, addMotorControl3);
+  ESPUI.setPanelWide(addMotorControl3, true);
+  addMotorControl4 = ESPUI.addControl(ControlType::Slider, "Additional Motor 4 Speed", String(addMotorSpeed4), Peterriver, configtab, addMotorControlCallback);
+  ESPUI.addControl(Min, "", "0", None, addMotorControl4);
+  ESPUI.addControl(Max, "", "4095", None, addMotorControl4);
+  ESPUI.setPanelWide(addMotorControl4, true);
   boardAction = ESPUI.addControl(Button,"Maintainance","Restart ESP32",ControlColor::Turquoise,configtab,espActionCallback);
   ESPUI.addControl(Button,"","Reset PCA9685",ControlColor::None,boardAction,espActionCallback);
   auto UltraSentab = ESPUI.addControl(Tab, "Ultrasonic", "Ultrasonic");
@@ -201,14 +215,14 @@ void additionalMotor(unsigned int motor, int val) {
     case 3:
     switch(val) {
       case 1:
-      pwm.setPWM(PWM_CHANNEL5,0,addMotorSpeed);
+      pwm.setPWM(PWM_CHANNEL5,0,addMotorSpeed3);
       pwm.setPWM(PWM_CHANNEL6,0,0);
       ESPUI.updateSwitcher(PWMMotor3s1,1);
       ESPUI.updateSwitcher(PWMMotor3s2,0);
       break;
       case -1:
       pwm.setPWM(PWM_CHANNEL5,0,0);
-      pwm.setPWM(PWM_CHANNEL6,0,addMotorSpeed);
+      pwm.setPWM(PWM_CHANNEL6,0,addMotorSpeed3);
       ESPUI.updateSwitcher(PWMMotor3s1,0);
       ESPUI.updateSwitcher(PWMMotor3s2,1);
       break;
@@ -223,14 +237,14 @@ void additionalMotor(unsigned int motor, int val) {
     case 4:
     switch(val) {
       case 1:
-      pwm.setPWM(PWM_CHANNEL7,0,addMotorSpeed);
+      pwm.setPWM(PWM_CHANNEL7,0,addMotorSpeed4);
       pwm.setPWM(PWM_CHANNEL8,0,0);
       ESPUI.updateSwitcher(PWMMotor4s1,1);
       ESPUI.updateSwitcher(PWMMotor4s2,0);
       break;
       case -1:
       pwm.setPWM(PWM_CHANNEL7,0,0);
-      pwm.setPWM(PWM_CHANNEL8,0,addMotorSpeed);
+      pwm.setPWM(PWM_CHANNEL8,0,addMotorSpeed4);
       ESPUI.updateSwitcher(PWMMotor4s1,0);
       ESPUI.updateSwitcher(PWMMotor4s2,1);
       break;
@@ -371,12 +385,16 @@ if (ps2x.ButtonReleased(PSB_PAD_LEFT) || ps2x.ButtonReleased(PSB_PAD_RIGHT)) {
 // In case someone managed to access the AP and gain access to config panel, the controller can press START button which will completely disable config tab for the entire session
 // and reset those value to default.
 void configtabDisable() {
-  ESPUI.setEnabled(addMotorControl, false);
+  ESPUI.setEnabled(addMotorControl3, false);
+  ESPUI.setEnabled(addMotorControl4, false);
   const String disabledstyle = "background-color: #bbb; border-bottom: #999 3px solid;";
-  ESPUI.setPanelStyle(addMotorControl, disabledstyle);
+  ESPUI.setPanelStyle(addMotorControl3, disabledstyle);
   ESPUI.updateVisibility(disabledWarning, true);
-  addMotorSpeed = 4095;
-  ESPUI.updateSlider(addMotorControl,addMotorSpeed);
+  ESPUI.setPanelStyle(addMotorControl4, disabledstyle);
+  addMotorSpeed4 = 4095;
+  addMotorSpeed3 = 3109;
+  ESPUI.updateSlider(addMotorControl3,addMotorSpeed3);
+  ESPUI.updateSlider(addMotorControl4,addMotorSpeed4);
   ESPUI.setEnabled(boardAction, false);
   ESPUI.setPanelStyle(boardAction, disabledstyle);
 }
